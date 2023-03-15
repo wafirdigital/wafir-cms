cc2eddb9aaed625d3b105036589d3dace210f78e
Odai Nasser <odai.karajah90@gmail.com>
2023-03-15 21:17:32 +0300

    registration route

--- app/Http/Controllers/Auth/UserAuthController.php
+++ app/Http/Controllers/Auth/UserAuthController.php
@@ -4,8 +4,47 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserAuthController extends Controller
 {
-    //
+     /**
+     * Registration
+     */
+    public function register(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|min:4',
+            'email' => 'required|email',
+            'password' => 'required|min:8',
+        ]);
+ 
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+       
+        $token = $user->createToken('LaravelAuthApp')->accessToken;
+ 
+        return response()->json(['token' => $token], 200);
+    }
+ 
+    /**
+     * Login
+     */
+    public function login(Request $request)
+    {
+        $data = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+ 
+        if (auth()->attempt($data)) {
+            $token = auth()->user()->createToken('LaravelAuthApp')->accessToken;
+            return response()->json(['token' => $token], 200);
+        } else {
+            return response()->json(['error' => 'Unauthorised'], 401);
+        }
+    }   
 }
