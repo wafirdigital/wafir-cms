c34ce28dbc9d59af4f5878d056c522d1e638ea43
Odai Nasser <odai.karajah90@gmail.com>
2023-03-18 22:14:10 +0300

    new migrations fields

--- database/migrations/2023_03_17_204138_create_teams_table.php
+++ database/migrations/2023_03_17_204138_create_teams_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('teams', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('display_name');
             $table->timestamps();
         });
     }
--- database/migrations/2023_03_17_204321_create_workspaces_table.php
+++ database/migrations/2023_03_17_204321_create_workspaces_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('workspaces', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('display_name');
             $table->timestamps();
         });
     }
