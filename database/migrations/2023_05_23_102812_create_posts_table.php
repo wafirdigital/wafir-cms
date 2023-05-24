73ab7349150a5ef603e3ccc5679ba179f3cfcbee
Odai Nasser <odai.karajah90@gmail.com>
2023-05-24 18:26:58 +0400

    adding posts migration

--- database/migrations/2023_05_23_102812_create_posts_table.php
+++ database/migrations/2023_05_23_102812_create_posts_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('discription');
+            $table->integer('user_id');
             $table->timestamps();
         });
     }
