7a005ec8b92de09e3fa6455b4bb6d3b8d609be5d
Odai Nasser <odai.karajah90@gmail.com>
2023-05-28 12:05:00 +0400

    update roles migration

--- database/migrations/2023_03_17_204148_create_roles_table.php
+++ database/migrations/2023_03_17_204148_create_roles_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->enum('type',['user','admin']);
             $table->string('discription');
             $table->string('slug');
             $table->timestamps();
