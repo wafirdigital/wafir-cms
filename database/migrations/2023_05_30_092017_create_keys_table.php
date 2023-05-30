b14366ece9de9b985555aaf3d91c7afd01433ab0
Odai Nasser <odai.karajah90@gmail.com>
2023-05-30 13:21:41 +0400

    remove posts module

--- database/migrations/2023_05_30_092017_create_keys_table.php
+++ database/migrations/2023_05_30_092017_create_keys_table.php
@@ -11,11 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('keys', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->string('discription');
-            $table->integer('user_id');
             $table->timestamps();
         });
     }
@@ -25,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('keys');
     }
 };
