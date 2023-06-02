3a97eec891487ad9d360870bda07ade05162254a
Odai Nasser <odai.karajah90@gmail.com>
2023-06-02 19:18:41 +0400

    prepare migration

--- database/migrations/2023_06_02_145259_create_bids_table.php
+++ database/migrations/2023_06_02_145259_create_bids_table.php
@@ -11,14 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('bids', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('type',['client','admin']);
-            $table->rememberToken();
+            $table->integer('product_id')->foreign('product_id')->references('id')->on('products');
+            $table->integer('user_id')->foreign('user_id')->references('id')->on('users');
+            $table->decimal('bid');
             $table->timestamps();
         });
     }
@@ -28,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('bids');
     }
 };
