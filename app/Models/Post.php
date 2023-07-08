5a78324fcbcbc636b8414a6f275cd33c83e7c016
Odai Nasser <karajah90@icloud.com>
2023-07-08 14:00:16 +0300

    fix updated by in model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -21,16 +21,16 @@ class Post extends Model
    
         static::creating(function ($model) {
             if (!$model->isDirty('created_by')) {
-                $model->created_by = !is_null(auth()->user()) ? auth()->user()->id : null;
+                $model->created_by = auth()->user()->id;
             }
             if (!$model->isDirty('updated_by')) {
-                $model->updated_by = !is_null(auth()->user()) ? auth()->user()->id : null;
+                $model->updated_by = auth()->user()->id;
             }
         });
 
         static::updating(function ($model) {
             if (!$model->isDirty('updated_by')) {
-                $model->updated_by = is_null(auth()->user()) ? auth()->user()->id : null;
+                $model->updated_by = auth()->user()->id;
             }
         });
     }
--- app/Models/User.php
+++ app/Models/User.php
@@ -49,19 +49,19 @@ class User extends Authenticatable
     {
 
         parent::boot();
-
+   
         static::creating(function ($model) {
             if (!$model->isDirty('created_by')) {
-                $model->created_by = !is_null(auth()->user()) ? auth()->user()->id : null;
+                $model->created_by = auth()->user()->id;
             }
             if (!$model->isDirty('updated_by')) {
-                $model->updated_by = !is_null(auth()->user()) ? auth()->user()->id : null;
+                $model->updated_by = auth()->user()->id;
             }
         });
 
         static::updating(function ($model) {
             if (!$model->isDirty('updated_by')) {
-                $model->updated_by = is_null(auth()->user()) ? auth()->user()->id : null;
+                $model->updated_by = auth()->user()->id;
             }
         });
     }
